17f5f3d7a2325c5b520b2241d52cc770d4781836
KitoMC <95675413+KitoMine@users.noreply.github.com>
2024-02-01 17:02:30 +0700

    Upload

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,13 @@
 function About() {
-  return <h1>About</h1>;
+  return (
+    <div className='font-bold text-neutral-800 w-full pb-4'>
+      <div className='mb-3 flex text-3xl gap-2 font-bold'>
+        <div className='bg-neutral-800 h-[36px] w-2'></div>
+        <h2>About 💤</h2>
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
 export default About;
--- src/pages/Specs/index.js
+++ src/pages/Specs/index.js
@@ -48,7 +48,7 @@ function Specs() {
   );
 
   return (
-    <div className='font-bold text-neutral-800 w-full'>
+    <div className='font-bold text-neutral-800 w-full pb-4'>
       <div className='mb-3 flex text-3xl gap-2 font-bold'>
         <div className='bg-neutral-800 h-[36px] w-2'></div>
         <h2>Spacs 💼</h2>
