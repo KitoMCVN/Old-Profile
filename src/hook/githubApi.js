8da25767b0c4165d29ad17f71ec39aadf2160190
KitoMC <95675413+KitoMine@users.noreply.github.com>
2024-02-10 15:57:23 +0700

    🎞 Repo card and projects page

--- src/hook/githubApi.js
+++ src/hook/githubApi.js
@@ -1,18 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCodeFork, faStar, faBook } from "@fortawesome/free-solid-svg-icons";
+import { faHtml5, faJs, faPython, faJava, faPhp, faSwift, faCss3 } from "@fortawesome/free-brands-svg-icons";
 
 const GitHubProjects = () => {
   const [projects, setProjects] = useState([]);
   const [blacklist] = useState(["KitoMCVN"]);
 
   useEffect(() => {
-    fetch("https://api.github.com/users/KitoMCVN/repos")
+    fetch("https://api.github.com/users/facebook/repos")
       .then((response) => response.json())
       .then((data) => {
         setProjects(data);
       })
       .catch((error) => console.error("Lỗi:", error));
   }, []);
+
+  const langIcon = {
+    JavaScript: <FontAwesomeIcon icon={faJs} />,
+    HTML: <FontAwesomeIcon icon={faHtml5} />,
+    Python: <FontAwesomeIcon icon={faPython} />,
+    Java: <FontAwesomeIcon icon={faJava} />,
+    PHP: <FontAwesomeIcon icon={faPhp} />,
+    Swift: <FontAwesomeIcon icon={faSwift} />,
+    CSS: <FontAwesomeIcon icon={faCss3} />,
+  };
 
   const filteredProjects = projects.filter((project) => !blacklist.includes(project.name));
 
@@ -21,10 +34,25 @@ const GitHubProjects = () => {
       {filteredProjects.map((project) => {
         return (
           <Link to={`/projects/${project.name}`} key={project.name}>
-            <div className='p-6 rounded-xl bg-slate-100'>
-              <div>{project.name}</div>
-              <p>{project.stargazers_count}</p>
-              <p>{project.forks_count}</p>
+            <div className='p-6 rounded-xl bg-slate-100 h-full'>
+              <div className='flex gap-2 items-center text-cyan-600 mb-2'>
+                <FontAwesomeIcon icon={faBook} />
+                <p>{project.name}</p>
+              </div>
+              <p className='text-sm truncate overflow-hidden text-ellipsis'>Mô tả: {project.description}</p>
+              <div className='flex gap-3 text-sm'>
+                {project.language && (
+                  <p>
+                    {langIcon[project.language]} {project.language}
+                  </p>
+                )}
+                <p>
+                  <FontAwesomeIcon icon={faCodeFork} /> {project.forks_count}
+                </p>
+                <p>
+                  <FontAwesomeIcon icon={faStar} /> {project.stargazers_count}
+                </p>
+              </div>
             </div>
           </Link>
         );
--- src/pages/Projects/projectsInfo.js
+++ src/pages/Projects/projectsInfo.js
@@ -1,26 +1,60 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+import "./readme.scss";
 
 const ProjectDetails = () => {
   const { projectName } = useParams();
   const [project, setProject] = useState(null);
+  const [readme, setReadme] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fetchReadme = async (projectUrl) => {
+    try {
+      const response = await fetch(`https://raw.githubusercontent.com/facebook/${projectName}/main/README.md`);
+      const data = await response.text();
+      const formattedReadme = <ReactMarkdown>{data}</ReactMarkdown>;
+      setReadme(formattedReadme);
+    } catch (error) {
+      setError(error);
+    }
+  };
 
   useEffect(() => {
-    fetch(`https://api.github.com/repos/KitoMCVN/${projectName}`)
-      .then((response) => response.json())
-      .then((data) => setProject(data))
-      .catch((error) => console.error(`Lỗi ${projectName}:`, error));
-  }, [projectName]);
+    const fetchProjectDetails = async () => {
+      try {
+        const response = await fetch(`https://api.github.com/repos/facebook/${projectName}`);
+        const data = await response.json();
+        setProject(data);
+        await fetchReadme(data.html_url);
+      } catch (error) {
+        setError(error);
+      }
+    };
 
+    fetchProjectDetails();
+  }, [projectName]);
   return (
-    <div>
-      <h2>Project Details: {projectName}</h2>
-      {project ? (
+    <div className='w-full'>
+      <h2>Project: {projectName}</h2>
+      {error ? (
+        <p>Lỗi: {error.message}</p>
+      ) : project ? (
         <div>
-          <p>Description: {project.description}</p>
+          <p>Mô tả: {project.description}</p>
+          {readme ? (
+            <>
+              <h3>README</h3>
+              <div className='p-6 rounded-xl bg-slate-100 w-full'>
+                <div className='readme'>{readme}</div>
+              </div>
+            </>
+          ) : (
+            <p>Loading README...</p>
+          )}
         </div>
       ) : (
-        <p>Loading...</p>
+        <p>Loading project details...</p>
       )}
     </div>
   );
