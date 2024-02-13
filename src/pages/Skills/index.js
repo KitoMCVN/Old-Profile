2178cee07458c2f612ff4098e6ae40f87b3d7cf5
KitoMC <95675413+KitoMine@users.noreply.github.com>
2024-02-13 11:31:09 +0700

    🔩 Update Skills Page

--- src/pages/Skills/index.js
+++ src/pages/Skills/index.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar as brandStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as regularStar } from "@fortawesome/free-regular-svg-icons";
 import htmlIcon from "../../assets/icons/html.svg";
 import cssIcon from "../../assets/icons/css.svg";
 import javascriptIcon from "../../assets/icons/javascript.svg";
@@ -20,25 +23,133 @@ function Skills() {
     {
       name: "Ngôn ngữ",
       contents: [
-        { img: htmlIcon, content: "HTML: (Ngôn ngữ đánh dấu siêu văn bản) là khối xây dựng 🧱 của trang web, sử dụng thẻ để xác định cấu trúc và nội dung." },
-        { img: cssIcon, content: "CSS: Ngôn ngữ kiểu tạo bố cục và trình bày cho HTML, giúp nhà phát triển thiết kế trang web hấp dẫn về mặt hình ảnh 🎨." },
-        { img: javascriptIcon, content: "JavaScript: Ngôn ngữ linh hoạt tạo tính tương tác ✨ và động 🥏 cho trang web, mang lại trải nghiệm hấp dẫn cho người dùng." },
+        {
+          img: htmlIcon,
+          content: "HTML: (Ngôn ngữ đánh dấu siêu văn bản) là khối xây dựng 🧱 của cách trang web, sử dụng các thẻ để xác định cấu trúc và nội dung.",
+          level: (
+            <>
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={regularStar} />
+              <FontAwesomeIcon icon={regularStar} />
+            </>
+          ),
+        },
+        {
+          img: cssIcon,
+          content: "CSS: (Cascading Style Sheets) Ngôn ngữ kiểu tạo bố cục và trình bày cho HTML, giúp nhà phát triển thiết kế trang web hấp dẫn về mặt hình ảnh 🎨.",
+          level: (
+            <>
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={regularStar} />
+              <FontAwesomeIcon icon={regularStar} />
+            </>
+          ),
+        },
+        {
+          img: javascriptIcon,
+          content: "JavaScript: Ngôn ngữ linh hoạt tạo tính tương tác ✨ và động 🥏 cho trang web, mang lại trải nghiệm hấp dẫn cho người dùng.",
+          level: (
+            <>
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={regularStar} />
+              <FontAwesomeIcon icon={regularStar} />
+              <FontAwesomeIcon icon={regularStar} />
+              <FontAwesomeIcon icon={regularStar} />
+            </>
+          ),
+        },
       ],
     },
     {
       name: "Frameworks & Styling",
       contents: [
-        { img: tailwindIcon, content: "TailwindCSS: Framework CSS ưu tiên tiện ích cho phép các nhà phát triển nhanh chóng xây dựng giao diện người dùng mà không cần rời khỏi HTML của mình" },
-        { img: reactIcon, content: "ReactJS: Thư viện dành cho giao diện người dùng gốc và web. Xây dựng giao diện người dùng từ các phần riêng lẻ được gọi là các component được viết bằng JavaScript" },
+        {
+          img: tailwindIcon,
+          content: "TailwindCSS: Framework CSS ưu tiên tiện ích cho phép các nhà phát triển nhanh chóng xây dựng giao diện người dùng mà không cần rời khỏi HTML của mình",
+          level: (
+            <>
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={regularStar} />
+              <FontAwesomeIcon icon={regularStar} />
+            </>
+          ),
+        },
+        {
+          img: reactIcon,
+          content: "ReactJS: Thư viện dành cho giao diện người dùng gốc và web. Xây dựng giao diện người dùng từ các phần riêng lẻ được gọi là các component được viết bằng JavaScript",
+          level: (
+            <>
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={regularStar} />
+              <FontAwesomeIcon icon={regularStar} />
+              <FontAwesomeIcon icon={regularStar} />
+              <FontAwesomeIcon icon={regularStar} />
+            </>
+          ),
+        },
       ],
     },
     {
       name: "Công cụ phát triển",
       contents: [
-        { img: githubIcon, content: "Github: Nền tảng cung cấp các công cụ cộng tác và kiểm soát phiên bản, cho phép các nhà phát triển lưu trữ 👩‍💻, đánh giá và quản lý kho mã cho các dự án phần mềm. " },
-        { img: vscodeIcon, content: "Visual Studio Code: Một công cụ miễn phí ✨ và mã nguồn mở được phát triển bởi Microsoft. Nó cung cấp nhiều tính năng và tiện ích mở rộng cho nhiều ngôn ngữ lập trình khác nhau" },
-        { img: nodejsIcon, content: "NodeJS: Một môi trường chạy JavaScript phía máy chủ 🖥, giúp tạo ra ứng dụng web có khả năng mở rộng và hiệu quả." },
-        { img: notepadppIcon, content: "Note Pad++: Trình soạn thảo văn bản 📝 mã nguồn mở và miễn phí dành cho Windows. Đây là một công cụ nhẹ và nhìu tính năng" },
+        {
+          img: githubIcon,
+          content: "Github: Nền tảng cung cấp các công cụ cộng tác và kiểm soát phiên bản, cho phép các nhà phát triển lưu trữ 👩‍💻, đánh giá và quản lý kho mã cho các dự án phần mềm.",
+          level: (
+            <>
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={regularStar} />
+              <FontAwesomeIcon icon={regularStar} />
+              <FontAwesomeIcon icon={regularStar} />
+            </>
+          ),
+        },
+        {
+          img: vscodeIcon,
+          content: "Visual Studio Code: Một công cụ miễn phí ✨ và mã nguồn mở được phát triển bởi Microsoft. Nó cung cấp nhiều tính năng và tiện ích mở rộng cho nhiều ngôn ngữ lập trình khác nhau",
+          level: (
+            <>
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={regularStar} />
+            </>
+          ),
+        },
+        {
+          img: nodejsIcon,
+          content: "NodeJS: Một môi trường chạy JavaScript phía máy chủ 🖥, giúp tạo ra ứng dụng web có khả năng mở rộng và hiệu quả.",
+          level: (
+            <>
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={regularStar} />
+              <FontAwesomeIcon icon={regularStar} />
+              <FontAwesomeIcon icon={regularStar} />
+              <FontAwesomeIcon icon={regularStar} />
+            </>
+          ),
+        },
+        {
+          img: notepadppIcon,
+          content: "Note Pad++: Trình soạn thảo văn bản 📝 mã nguồn mở và miễn phí dành cho Windows. Đây là một công cụ nhẹ và nhìu tính năng",
+          level: (
+            <>
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={brandStar} />
+              <FontAwesomeIcon icon={brandStar} />
+            </>
+          ),
+        },
       ],
     },
   ];
@@ -52,11 +163,11 @@ function Skills() {
       <p>Kỹ năng và công nghệ mà tui sử dụng để xây dựng mọi thứ. 😊</p>
       <div className='mt-6'>
         {categories.map((category, index) => (
-          <div className='border-b-slate-600/40 border-b-[1px] cursor-pointer transition-all duration-300'>
+          <div className='border-b-slate-600/40 border-b-[1px] cursor-pointer'>
             <div key={index}>
               <div className='pt-6 pb-4' onClick={() => toggleCategory(index)}>
                 <h2 className='text-lg'>
-                  {category.name} {openCategory === index ? "-" : "+"}
+                  {category.name} {openCategory === index ? <span className='text-cyan-600'>-</span> : "+"}
                 </h2>
               </div>
               {openCategory === index && (
@@ -66,8 +177,9 @@ function Skills() {
                       <div className='size-14 min-w-14 rounded-sm overflow-hidden'>
                         <img className='size-full' src={item.img} alt={category.name} />
                       </div>
-                      <div className='w-full hover:*:whitespace-normal overflow-hidden'>
+                      <div className='w-full group hover:*:whitespace-normal overflow-hidden'>
                         <p className='truncate transition-all'>{item.content}</p>
+                        <p className='group-hover:hidden text-sm'>{item.level}</p>
                       </div>
                     </div>
                   ))}
